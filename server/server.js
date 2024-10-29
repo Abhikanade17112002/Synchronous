1db3d4bcec15b49d5d38431811c97faa2d813c69
Abhishek Kanade <abhishekrangnathkanade21@gmail.com>
2024-10-29 14:48:32 +0530

    prod

--- server/server.js
+++ server/server.js
@@ -8,9 +8,11 @@ const ContactsRouter = require("./routes/contacts.routes");
 const MessagesRouter = require("./routes/messages.route");
 const setUpSocketIO = require("./socket.io/socket");
 const channelRouter = require("./routes/channel.routes");
+const path = require("path") ;
 connectToDatabase()
   .then((connectionInstance) => {
     const App = express();
+    const __dirname = path.resolve() ;
 
     // Middleware
     App.use(express.json());
@@ -33,15 +35,22 @@ connectToDatabase()
     App.use("/api/contacts", ContactsRouter);
     App.use("/api/messages", MessagesRouter);
     App.use("/api/channels", channelRouter);
-
+    App.use(express.static(path.join(__dirname,"/client/dist")));
+    App.get("*", (request, response) => {
+      response.sendFile(path.join(__dirname,"/client/dist/index.html"));
+    })
     App.get("/", (request, response) => {
       response.send("Hello World");
     });
-
+   
     const server = App.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
     setUpSocketIO(server);
+    
+    
+
+   
   })
   .catch((error) => {
     console.log("====================================");
